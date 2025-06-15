b3daa99b5224bad26a3cb4d6d5de052f3bae0030
richen604 <richen604@users.noreply.github.com>
2025-06-15 00:29:19 +0000

    chore(deps): update flake.lock

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Hydenix's nixpkgs
-    hydenix-nixpkgs.url = "github:nixos/nixpkgs/12a55407652e04dcf2309436eb06fef0d3713ef3";
+    hydenix-nixpkgs.url = "github:nixos/nixpkgs/6afe187897bef7933475e6af374c893f4c84a293";
 
     home-manager = {
       url = "github:nix-community/home-manager";
