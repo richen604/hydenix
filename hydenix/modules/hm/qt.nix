ed637f776422e828dd9e4ce4831ab2d4c81b5c73
richen604 <56615615+richen604@users.noreply.github.com>
2025-10-01 16:26:46 -0700

    feat: qt5 eol. removed hydenix.sddm.theme, added sddm astronaut theme

--- hydenix/modules/hm/qt.nix
+++ hydenix/modules/hm/qt.nix
@@ -19,12 +19,6 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
-      libsForQt5.qt5ct
-      libsForQt5.qt5.qtbase
-      libsForQt5.qtstyleplugin-kvantum
-      libsForQt5.qtimageformats
-      libsForQt5.qtsvg
-      libsForQt5.qt5.qtwayland
       kdePackages.qt6ct
       kdePackages.qtbase
       kdePackages.qtwayland
--- hydenix/modules/system/sddm.nix
+++ hydenix/modules/system/sddm.nix
@@ -15,21 +15,13 @@ in
       default = true;
       description = "Enable sddm module";
     };
-
-    theme = lib.mkOption {
-      type = lib.types.enum [
-        "Candy"
-        "Corners"
-      ];
-      default = "Candy";
-      description = "SDDM theme package to use";
-    };
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [
       pkgs.hydenix.hyde
       pkgs.Bibata-Modern-Ice
+      pkgs.sddm-astronaut
     ];
 
     # Add this section to ensure cursor theme is properly loaded
@@ -40,25 +32,17 @@ in
 
     services.displayManager.sddm = {
       enable = true;
-      package = pkgs.libsForQt5.sddm;
-      theme = pkgs.hydenix.hyde + "/share/sddm/themes/" + cfg.theme;
+      theme = "sddm-astronaut-theme";
       wayland = {
         enable = true;
       };
-      extraPackages = with pkgs; [
-        libsForQt5.sddm
-        libsForQt5.qtsvg
-        libsForQt5.qtmultimedia
-        libsForQt5.qtvirtualkeyboard
-        libsForQt5.qtquickcontrols2
-        libsForQt5.qtgraphicaleffects
-        libsForQt5.qt5.qtwayland
-        hydenix.hyde
-        Bibata-Modern-Ice
+      extraPackages = with pkgs.kdePackages; [
+        qtsvg
+        qtmultimedia
+        qtvirtualkeyboard
       ];
       settings = {
         Theme = {
-          ThemeDir = "/run/current-system/sw/share/sddm/themes";
           CursorTheme = "Bibata-Modern-Ice";
           CursorSize = "24";
         };
