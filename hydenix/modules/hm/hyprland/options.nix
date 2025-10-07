8dda1cb857467d331fb7e6b13a6636794f2a7018
Richard Henninger <56615615+richen604@users.noreply.github.com>
2025-10-06 23:35:05 -0700

    Revert "feat: pyprland module options for hyprland"
    This reverts commit 6175f42063dc292cc317ea29b1692ce7b49b47df.

--- hydenix/modules/hm/hyprland/options.nix
+++ hydenix/modules/hm/hyprland/options.nix
@@ -185,23 +185,6 @@ in
       };
     };
 
-    # Pyprland configurations
-    pyprland = {
-      enable = lib.mkEnableOption "pyprland configurations" // {
-        default = cfg.enable;
-      };
-      extraConfig = lib.mkOption {
-        type = lib.types.lines;
-        default = "";
-        description = "Additional pyprland configuration";
-      };
-      overrideConfig = lib.mkOption {
-        type = lib.types.nullOr lib.types.lines;
-        default = null;
-        description = "Complete pyprland configuration override";
-      };
-    };
-
     # Monitor configurations
     monitors = {
       enable = lib.mkEnableOption "monitor configurations" // {
